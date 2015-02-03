49fb5fb7015c33c4ff15be6fc2e0e3227294e91c
JrmyDev <jeremy.marquer@reseau.eseo.fr>
2015-02-03 12:05:11 +0100

    Activation/desactivation service administration ok

--- app/src/main/java/fr/eseo/safemyphone/MainActivity.java
+++ app/src/main/java/fr/eseo/safemyphone/MainActivity.java
@@ -1,23 +1,38 @@
 package fr.eseo.safemyphone;
 
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
 
 public class MainActivity extends ActionBarActivity {
     Button buttonPref;
+    Switch switch1;
     Intent intent;
+    static final String TAG = "DeviceAdminSample";
+    static final int ACTIVATION_REQUEST = 47; // identifies our request id
+    DevicePolicyManager devicePolicyManager;
+    ComponentName demoDeviceAdmin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnClickListener(myhandler2);
         buttonPref = (Button) findViewById(R.id.preference);
         buttonPref.setOnClickListener(myhandler1);
+
     }
     View.OnClickListener myhandler1 = new View.OnClickListener() {
         public void onClick(View v) {
@@ -26,8 +41,42 @@ public class MainActivity extends ActionBarActivity {
             startActivity(intent);
         }
     };
+    View.OnClickListener myhandler2 = new View.OnClickListener() {
+        public void onClick(View v) {
+            activateService();
+            if (switch1.isChecked()) {
+                // Activate device administration
+                Intent intent = new Intent(
+                        DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+                        demoDeviceAdmin);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                        "Your boss told you to do this");
+                startActivityForResult(intent, ACTIVATION_REQUEST);
+            }
+            Log.d(TAG, "onCheckedChanged to: " + switch1.isChecked());
 
+        }
+    };
 
+    private void activateService() {
+        devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        demoDeviceAdmin = new ComponentName(this, DeviceAdminSample.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ACTIVATION_REQUEST:
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.i("DeviceAdminSample", "Administration enabled!");
+                } else {
+                    Log.i("DeviceAdminSample", "Administration enable FAILED!");
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
